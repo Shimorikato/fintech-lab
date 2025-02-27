6c1a13d82b05c1fa3a9b26220e7654cf8b466988
Shimorikato <140848344+Shimorikato@users.noreply.github.com>
2025-02-27 16:58:06 +0530

    Delete lab4 directory

--- lab4/demo/src/main/java/com/b1/demo/repository/CustomerProofOfIDRepository.java
+++
@@ -1,9 +0,0 @@
-package com.b1.demo.repository;
-import com.b1.demo.entity.CustomerProofOfID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.b1.demo.entity.CustomerDetail;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerProofOfIDRepository extends JpaRepository<CustomerProofOfID, Long> {}
